e105ecc1c17bc89a0c28b9950771344648955c9f
Sergei Lebedev <superbobry@gmail.com>
2011-11-07 17:55:50 +0400

    Added -author() to idna.erl

--- src/idna.erl
+++ src/idna.erl
@@ -1,5 +1,11 @@
+%% @doc Punycode implementation for Erlang.
+
 -module(idna).
+-author("Tim Fletcher <tim@tfletcher.com>").
+-author("Sergei Lebedev <superbobry@gmail.com>").
+
 -behaviour(application).
+
 -export([start/2, stop/1]).
 -export([start/0, stop/0, to_ascii/1, from_ascii/1]).
 
@@ -9,11 +15,11 @@
 
 -spec to_ascii(nonempty_string()) -> nonempty_string().
 to_ascii(Domain) ->
-  to_ascii(string:tokens(ux_string:to_lower(Domain), "."), []).
+    to_ascii(string:tokens(ux_string:to_lower(Domain), "."), []).
 
 -spec from_ascii(nonempty_string()) -> nonempty_string().
 from_ascii(Domain) ->
-  from_ascii(string:tokens(Domain, "."), []).
+    from_ascii(string:tokens(Domain, "."), []).
 
 -spec start() -> ok.
 start() ->
@@ -33,11 +39,11 @@ stop(_State) ->
 %% Helper functions
 
 to_ascii([], Acc) ->
-  lists:reverse(Acc);
+    lists:reverse(Acc);
 to_ascii([Label|Labels], []) ->
-  to_ascii(Labels, lists:reverse(label_to_ascii(Label)));
+    to_ascii(Labels, lists:reverse(label_to_ascii(Label)));
 to_ascii([Label|Labels], Acc) ->
-  to_ascii(Labels, lists:reverse(label_to_ascii(Label), [$.|Acc])).
+    to_ascii(Labels, lists:reverse(label_to_ascii(Label), [$.|Acc])).
 
 label_to_ascii(Label) ->
     case lists:all(fun(C) -> xmerl_ucs:is_ascii(C) end, Label) of
@@ -47,11 +53,11 @@ label_to_ascii(Label) ->
     end.
 
 from_ascii([], Acc) ->
-  lists:reverse(Acc);
+    lists:reverse(Acc);
 from_ascii([Label|Labels], []) ->
-  from_ascii(Labels, lists:reverse(label_from_ascii(Label)));
+    from_ascii(Labels, lists:reverse(label_from_ascii(Label)));
 from_ascii([Label|Labels], Acc) ->
-  from_ascii(Labels, lists:reverse(label_from_ascii(Label), [$.|Acc])).
+    from_ascii(Labels, lists:reverse(label_from_ascii(Label), [$.|Acc])).
 
 label_from_ascii(?ACE_PREFIX ++ Label) ->
 	punycode:decode(Label);
